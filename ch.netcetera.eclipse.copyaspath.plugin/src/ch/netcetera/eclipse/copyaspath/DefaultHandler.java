865d686a200bb9fd93dd958afd5b4c550ce21846
Thomas Mäder <github@devotek-it.ch>
2013-09-13 09:47:18 +0000

    Added some branding

--- ch.netcetera.eclipse.copyaspath.plugin/src/ch/netcetera/eclipse/copyaspath/DefaultHandler.java
+++ ch.netcetera.eclipse.copyaspath.plugin/src/ch/netcetera/eclipse/copyaspath/DefaultHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 Netcetera AG and others.
+ * Copyright (c) 2013 Netcetera AG.
  * All rights reserved.
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License v1.0 which accompanies this
